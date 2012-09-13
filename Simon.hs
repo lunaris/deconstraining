d0079fa8ebc1c349190f33bdf81dc356fe3d44a3
Will Jones <will@sacharissa.co.uk>
2012-09-13 10:39:57 +0100

    Kick it up a notch. Bam.

--- Simon.hs
+++ Simon.hs
@@ -19,6 +19,8 @@
 
 module Simon where
 
+import GHC.Prim
+
 data Exp1 c a where
   ValueE1 :: c => a -> Exp1 c a
   CondE1  :: Exp1 c Bool -> Exp1 c a -> Exp1 c a -> Exp1 c a
@@ -114,9 +116,6 @@ exp2ToCuda (EqE2 e1 e2)
 exp2ToCuda (AddE2 e1 e2)
   = "(Add " ++ exp2ToCuda e1 ++ " " ++ exp2ToCuda e2 ++ ")"
 
-data Dict c a where
-  Dict :: c a => Dict c a
-
 pickLeft :: Exp2 (Both c d) a -> Exp2 c a
 pickLeft (ValueE2 x)
   = ValueE2 x
@@ -143,5 +142,104 @@ pickRight (EqE2 e1 e2)
 pickRight (AddE2 e1 e2)
   = AddE2 (pickRight e1) (pickRight e2)
 
-f e
+f2 e
   = (evaluate2 (pickLeft e), exp2ToCuda (pickRight e))
+
+data Dict c a where
+  Dict :: c a => Dict c a
+
+data Proxy (cs :: [* -> Constraint])
+  = Proxy
+  deriving (Eq, Show)
+
+class ApplyAll cs a
+  =>  AllOf (cs :: [* -> Constraint]) (a :: *) where
+
+  type ApplyAll cs a  :: Constraint
+
+  pickOne             :: Elem c cs
+                      => Proxy cs -> (Dict c a -> b) -> b
+
+instance AllOf '[] a where
+  type ApplyAll '[] a
+    = ()
+
+  pickOne Proxy (f :: Dict c a -> b)
+    = seq (evidence :: Evidence c '[]) (error "Impossible")
+
+instance (c a, AllOf cs a) => AllOf (c ': cs) a where
+  type ApplyAll (c ': cs) a
+    = (c a, ApplyAll cs a)
+
+  pickOne Proxy (f :: Dict d a -> b)
+    = case evidence :: Evidence d (c ': cs) of
+        Head -> f Dict
+        Tail -> pickOne (Proxy :: Proxy cs) f
+
+data Exp3 cs a where
+  ValueE3 :: AllOf cs a => a -> Exp3 cs a
+
+  CondE3  :: (AllOf cs a, AllOf cs Bool)
+          => Exp3 cs Bool -> Exp3 cs a -> Exp3 cs a -> Exp3 cs a
+
+  EqE3    :: (Eq a, AllOf cs a, AllOf cs Bool)
+          => Exp3 cs a -> Exp3 cs a -> Exp3 cs Bool
+
+  AddE3   :: (Num a, AllOf cs a)
+          => Exp3 cs a -> Exp3 cs a -> Exp3 cs a
+
+instance (Num a, AllOf cs a) => Num (Exp3 cs a) where
+  (+)
+    = AddE3
+
+  fromInteger
+    = ValueE3 . fromInteger
+
+evaluate3 :: Exp3 cs a -> a
+evaluate3 (ValueE3 x)
+  = x
+
+evaluate3 (CondE3 p t f)
+  = if evaluate3 p then evaluate3 t else evaluate3 f
+
+evaluate3 (EqE3 e1 e2)
+  = evaluate3 e1 == evaluate3 e2
+
+evaluate3 (AddE3 e1 e2)
+  = evaluate3 e1 + evaluate3 e2
+
+class Elem x xs where
+  evidence :: Evidence x xs
+
+instance Elem x (x ': xs) where
+  evidence
+    = Head
+
+instance Elem x xs => Elem x (y ': xs) where
+  evidence
+    = Tail
+
+data Evidence x xs where
+  Head :: Evidence x (x ': xs)
+  Tail :: (Elem x xs) => Evidence x (y ': xs)
+
+exp3ToCuda :: forall a cs. Elem Cuda cs => Exp3 cs a -> String
+exp3ToCuda (ValueE3 x)
+  = pickOne (Proxy :: Proxy cs) (f x)
+    where
+      f :: a -> Dict Cuda a -> String
+      f x Dict
+        = toCuda x
+
+exp3ToCuda (CondE3 p t f)
+  = "(Cond " ++ exp3ToCuda p ++ " " ++ exp3ToCuda t ++ " " ++
+      exp3ToCuda f ++ ")"
+
+exp3ToCuda (EqE3 e1 e2)
+  = "(Eq " ++ exp3ToCuda e1 ++ " " ++ exp3ToCuda e2 ++ ")"
+
+exp3ToCuda (AddE3 e1 e2)
+  = "(Add " ++ exp3ToCuda e1 ++ " " ++ exp3ToCuda e2 ++ ")"
+
+f3 e
+  = (evaluate3 e, exp3ToCuda e)
